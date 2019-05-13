a24ad8af3c333aca62781d88caffff06c0867cab
Fabian Olsson <fabtasticdesign@live.se>
2019-05-13 10:38:14 +0200

    init

---
+++ myfirstplugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* Plugin Name: My First Plugin
+* Plugin URI: http://fabianolsson.se
+* Description: this you do and then dis lol
+* Version: 0.01
+* Author: fo
+* License: wtfpl
+* License URI: http://wtfpl.net
+* Text Domain: myfirstplugin
+* Domain Path: languages/
+*/
+
